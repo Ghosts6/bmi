fc70e86cf09fdcb3dca3be80a3a8b85bb65dfe6c
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-03-19 12:58:06 +0000

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,4 +1,3 @@
-# bmi with python3
 # this program want to check your bmi and health stats
 class MyInfo:
     weight = int()
@@ -6,17 +5,46 @@ class MyInfo:
     name = ""
     lname = ""
     pass
+    
 MyInfo.name = input("enter your name :")
 MyInfo.lname = input("enter your lastname:")
-MyInfo.weight = input("enter your weight(kg):")  # type: ignore
-MyInfo.stature = input("enter your stature (m):")  # type: ignore
-MyInfo.stature = int(MyInfo.stature) ;MyInfo.weight = int (MyInfo.weight)
-A = MyInfo.stature / 100 ; B = A * A
-bmi =MyInfo.weight / B
-print("mr/mrs",MyInfo.name,"your bmi equal with :",bmi)
+full_name = MyInfo.name + " " + MyInfo.lname
+MyInfo.weight = float(input("enter your weight(kg):"))  
+MyInfo.stature = float(input("enter your stature (m):")) 
+statureM = MyInfo.stature / 100 ; stature_power_by_2 = statureM * statureM
+bmi =MyInfo.weight / stature_power_by_2
+print("mr/mrs",full_name,"your bmi equal with :",bmi)
+
 if(bmi<=18.49):
     print("you are under normal weight")
+    status = "u"
 elif(18.5<=bmi and bmi<=24.49):
     print("you have normal weight")
-elif(24.5<=bmi):
+    status = "n"
+elif(25<=bmi and bmi <= 29.9):
     print("you have extra weight")
+    status = "x"
+elif(30 <= bmi):
+    print("bmi status : obesity")
+    status = "o"
+print("thank for your time we also have some advice about weight, do you wane see them ?(y/n):")
+choice = input("")
+if choice == "y" or choice == "Y":
+    if status == "u":
+        print("Eat more frequently. When you're underweight, you may feel full faster. Eat five to six smaller meals during the day rather than two or three large meals.Choose nutrient-rich foods. As part of an overall healthy diet, choose whole-grain breads, pastas and cereals; fruits and vegetables; dairy products; lean protein sources; and nuts and seeds")
+    elif status == "n":
+        print("Foods high in sugar and calories lead to weight gain if you are not careful. Choose foods that are high in nutrition and taste, like fruits, vegetables and whole grains. You dont have to sacrifice your favorite foods in order to take care of your body either it s ok to have a couple of cheat meals or cheat days every once in a while.")
+    elif status == "x":
+        print("Following a healthy eating plan with fewer calories is often the first step in trying to treat overweight and obesity")
+    elif status == "o":
+        print("Changing your eating and physical activity habits and lifestyle is difficult, but with a plan, effort, regular support, and patience, you may be able to lose weight and improve your health. The following tips may help you think about ways to lose weight, engage in regular physical activity, and improve health over the long-term")
+    else :
+        print("Error")
+else :
+    print("thanks for time")
+
+weight_lose = bmi - 18.5
+if weight_lose < 5 :
+    print("your weigh ok,you dont need lose much ")
+elif weight_lose > 5:
+    print("you need to lose", weight_lose, "to be healthy")
